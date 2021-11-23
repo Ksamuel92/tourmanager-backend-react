e27c34c44579cc7a9ce9d8af19d5ce069f5c3528
Kyle Samuel <ksamuel92@gmail.com>
2021-11-23 11:37:56 -0500

    flesh out show controller

--- app/controllers/show_controller.rb
+++ app/controllers/show_controller.rb
@@ -1,14 +1,30 @@
 class ShowController < ApplicationController
-  def index
-    shows = Show.all
-    render json: shows
+  def create
+    show = Show.new(show_params)
+    if show.save
+      render json: show
+    else
+      render json: { error: show.errors.messages }, status: 422
+    end
   end
 
-  def create; end
+  def update
+    show = Show.find_by(params[:id])
+    if show.update(show_params)
+      render json: show
+    else
+      render json: { error: show.errors.messages }, status: 422
+    end
+  end
 
-  def update; end
-
-  def destroy; end
+  def destroy
+    show = show.find_by(params[:id])
+    if show.destroy
+      head :no_content # Returns a response that has no content (merely headers). The options argument is interpreted to be a hash of header names and values.
+    else
+      render json: { error: show.errors.messages }, status: 422
+    end
+  end
 
   private
 
