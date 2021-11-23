70a204c77091306ecd1b085a6cdc7710d1ac121c
Kyle Samuel <ksamuel92@gmail.com>
2021-11-23 10:34:16 -0500

    add total_gross to show and slugify to promoter

--- app/models/promoter.rb
+++ app/models/promoter.rb
@@ -1,4 +1,18 @@
 class Promoter < ApplicationRecord\
   has_many :shows
   has_many :users, through: :shows
+  before_create :slugify
+
+  def slugify
+    self.slug = name.parameterize
+  end
 end
+
+
+# create_table "promoters", force: :cascade do |t|
+#   t.string "name"
+#   t.string "email"
+#   t.string "slug"
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+# end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,26 @@
 class Show < ApplicationRecord
   belongs_to :user
   belongs_to :promoter
+
+
+  def total_gross
+    self.merch + self.
+  end
 end
+
+
+# create_table "shows", force: :cascade do |t|
+#   t.string "venue"
+#   t.date "date"
+#   t.time "loadin"
+#   t.decimal "guarantee"
+#   t.decimal "merch"
+#   t.boolean "green_room"
+#   t.text "wifi"
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+#   t.bigint "user_id", null: false
+#   t.bigint "promoter_id", null: false
+#   t.index ["promoter_id"], name: "index_shows_on_promoter_id"
+#   t.index ["user_id"], name: "index_shows_on_user_id"
+# end
