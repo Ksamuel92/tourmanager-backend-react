ed5d25f7cae5cba2f52494b44b259a6022e995c5
Kyle Samuel <ksamuel92@gmail.com>
2021-11-23 14:55:31 -0500

    add index and show actions to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :shows, only: %i[index create update destroy]
-  resources :promoters, only: %i[index create update destroy], params: :slug
+  resources :shows, only: %i[index show create update destroy]
+  resources :promoters, only: %i[index show create update destroy], param: :slug
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
