6a435e7f640147b1de509be6a9a33240d7babed7
Kyle Samuel <ksamuel92@gmail.com>
2021-11-29 12:40:20 -0500

    set devise default format to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, defaults: { format: :json }
   resources :shows, only: %i[index show create update destroy]
   resources :promoters, only: %i[index show create update destroy], param: :slug
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
