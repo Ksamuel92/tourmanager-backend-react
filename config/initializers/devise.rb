b5fd4bdf94ba7b3a4c8328f6a828de1629baa726
Kyle Samuel <ksamuel92@gmail.com>
2021-11-29 14:24:10 -0500

    remove jwt_payload instance method

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,6 +271,13 @@ Devise.setup do |config|
   # Config for JSON Web Token Authentication
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 30.minutes.to_i
   end
 
   # ==> OmniAuth
