cf10c42ee0f21ed160d81716c3b716ecc58a35e0
Kyle Samuel <ksamuel92@gmail.com>
2021-12-12 22:06:33 -0500

    add city to serializer

--- app/serializers/show_serializer.rb
+++ app/serializers/show_serializer.rb
@@ -1,5 +1,5 @@
 class ShowSerializer < ActiveModel::Serializer
-  attributes :id, :venue, :date, :loadin, :guarantee, :merch, :green_room, :wifi
+  attributes :id, :venue, :city, :date, :loadin, :guarantee, :merch, :green_room, :wifi
   belongs_to :promoter
   belongs_to :user
 end
