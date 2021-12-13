63de132ddf2da30b6d3304445505b28611fa2c3d
Kyle Samuel <ksamuel92@gmail.com>
2021-12-12 22:07:27 -0500

    change guarantee and merch migration to integer

--- db/migrate/20211123152234_create_shows.rb
+++ db/migrate/20211123152234_create_shows.rb
@@ -2,10 +2,11 @@ class CreateShows < ActiveRecord::Migration[6.1]
   def change
     create_table :shows do |t|
       t.string :venue
+      t.string :city
       t.date :date
       t.time :loadin
-      t.decimal :guarantee
-      t.decimal :merch
+      t.integer :guarantee
+      t.integer :merch
       t.boolean :green_room
       t.text :wifi
 
