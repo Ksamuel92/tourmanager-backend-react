2e4843069ac2e7418a29bb552cb6173d3cc4ceec
Kyle Samuel <ksamuel92@gmail.com>
2021-12-12 22:05:55 -0500

    add validations for promoter

--- app/models/promoter.rb
+++ app/models/promoter.rb
@@ -2,6 +2,8 @@ class Promoter < ApplicationRecord
   has_many :shows
   has_many :users, through: :shows
   before_create :slugify
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
 
   def slugify
     self.slug = name.parameterize
