bdd4cd0e3fb5c5fc6247988a4e05f2e486e4dee7
Kyle Samuel <ksamuel92@gmail.com>
2022-01-01 14:52:24 -0600

    comment out CORs actions in appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :cors_set_access_control_headers
+  # before_action :cors_set_access_control_headers
 
-def cors_preflight_check
-  if request.method == 'OPTIONS'
-    cors_set_access_control_headers
-    render text: '', content_type: 'text/plain'
-  end
-end
+# def cors_preflight_check
+#   if request.method == 'OPTIONS'
+#     cors_set_access_control_headers
+#     render text: '', content_type: 'text/plain'
+#   end
+# end
 
 protected
 
@@ -16,10 +16,10 @@ def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 end
 
-def cors_set_access_control_headers
-  response.headers['Access-Control-Allow-Origin'] = 'https://tourmanager-frontend.herokuapp.com'
-  response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-  response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
-  response.headers['Access-Control-Max-Age'] = '1728000'
-end
+# def cors_set_access_control_headers
+#   response.headers['Access-Control-Allow-Origin'] = '*'
+#   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+#   response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+#   response.headers['Access-Control-Max-Age'] = '1728000'
+# end
 end
