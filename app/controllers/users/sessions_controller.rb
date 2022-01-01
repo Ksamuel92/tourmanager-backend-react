f5e6312cb1bc8dbee6623b8b0d2a993cef837981
Kyle Samuel <ksamuel92@gmail.com>
2022-01-01 14:13:17 -0600

    sanitize login params

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -44,8 +44,8 @@ class Users::SessionsController < Devise::SessionsController
     end
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:id, :email, :name])
+  end
 end
