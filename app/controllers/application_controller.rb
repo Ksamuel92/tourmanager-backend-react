5d09062625b9e7cfb20a0e44ee7f2f919e37180b
Kyle Samuel <ksamuel92@gmail.com>
2021-12-31 19:26:28 -0600

    remove backslash from cors headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ def configure_permitted_parameters
 end
 
 def cors_set_access_control_headers
-  response.headers['Access-Control-Allow-Origin'] = 'https://tourmanager-frontend.herokuapp.com/'
+  response.headers['Access-Control-Allow-Origin'] = 'https://tourmanager-frontend.herokuapp.com'
   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
   response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
   response.headers['Access-Control-Max-Age'] = '1728000'
