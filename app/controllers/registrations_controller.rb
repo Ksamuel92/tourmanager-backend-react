99db26e5cc1cd1e07468e2285b8eae7f0195e87d
Kyle Samuel <ksamuel92@gmail.com>
2022-01-04 20:21:31 -0600

    move devise controllers outside of API namespace

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  respond_to :json
+
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    sign_up(resource_name, resource) if resource.persisted?
+
+    render_jsonapi_response(resource)
+  end
 
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
@@ -42,20 +49,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   private
 
-  def respond_with(resource, _opts = {})
-    if resource.persisted?
-      render json: {
-        status: { code: 200, message: 'Signed up sucessfully.' },
-        data: ActiveModelSerializers::SerializableResource.new(resource)
-      }
-    else
-      render json: {
-        status: { message: "User failed to be created. #{resource.errors.full_messages.to_sentence}" }
-      }, status: :unprocessable_entity
-    end
-  end
+  # def respond_with(resource, _opts = {})
+  #   if resource.persisted?
+  #     render json: {
+  #       status: { code: 200, message: 'Signed up sucessfully.' },
+  #       data: ActiveModelSerializers::SerializableResource.new(resource)
+  #     }
+  #   else
+  #     render json: {
+  #       status: { message: "User failed to be created. #{resource.errors.full_messages.to_sentence}" }
+  #     }, status: :unprocessable_entity
+  #   end
+  # end
 
-  # protected
+  # # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
