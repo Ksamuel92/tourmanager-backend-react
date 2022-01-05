eb3dcda98cec9268cc1eb4c8a815ed7762283457
Kyle Samuel <ksamuel92@gmail.com>
2022-01-04 19:35:21 -0600

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'current_user', to: 'current_user#index'
-  match '*all', controller: 'application', action: 'cors_preflight_check', via: [:options]
+  root "shows#index"
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
